d054456dfd17751d1e6a3c3ec824ad9eb5fac7cf
Bhavya Sahu <bhavyaias001@gmail.com>
2020-08-14 01:05:45 +0530

    Updates

---
+++ precache-manifest.14fc211398f83043a004546d1b724278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c1e2204f75e2c0a34957fc784f6635",
+    "url": "/React-app-using-api/index.html"
+  },
+  {
+    "revision": "ead40615ca0f24cc82b7",
+    "url": "/React-app-using-api/static/css/main.34a97434.chunk.css"
+  },
+  {
+    "revision": "dda2b6a5cbea2fbf4296",
+    "url": "/React-app-using-api/static/js/2.9b2aa5ea.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/React-app-using-api/static/js/2.9b2aa5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead40615ca0f24cc82b7",
+    "url": "/React-app-using-api/static/js/main.ac15fa32.chunk.js"
+  },
+  {
+    "revision": "8fc9851f40089bcf2f37",
+    "url": "/React-app-using-api/static/js/runtime-main.50e8cef1.js"
+  },
+  {
+    "revision": "a10480f9e371778147da451d9d9985f3",
+    "url": "/React-app-using-api/static/media/500_F_331001873_c8T80H4hJ3Ag6xYwdSB1pbZpn025Vzbx.a10480f9.jpg"
+  },
+  {
+    "revision": "833344f7bf1eac23e7f12d58ed8c808e",
+    "url": "/React-app-using-api/static/media/c.833344f7.jpg"
+  }
+]);
